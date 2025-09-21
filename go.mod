42e961a9271018eba166670bff306426887b7906
Jdsatashi <vdt1073@gmail.com>
2025-09-21 12:03:16 +0700

    feat: add string upper, capital case

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ComepassDeveloper/jd-utils
 
 go 1.25.0
 
-require github.com/chai2010/webp v1.4.0
+require (
+	github.com/chai2010/webp v1.4.0
+	golang.org/x/text v0.29.0
+)
---
+++ pkg/jstr/upper.go
@@ -0,0 +1,39 @@
+package jstr
+
+import (
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+// Function to convert string to title case (e.g. "hello world" -> "Hello World")
+func Title(s string) string {
+	cases := cases.Title(language.Und)
+	return cases.String(s)
+}
+
+// Function to convert string to sentence case (e.g. "hello world" -> "Hello world")
+func Capitalize(s string) string {
+	if s == "" {
+		return s
+	}
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
+}
+
+// Function to convert snake_case to variable camelCase (e.g. "hello_world" -> "helloWorld")
+func VariableCamel(s string) string {
+	var result string
+	comp := strings.Split(s, "_")
+	for i, v := range comp {
+		if i == 0 {
+			result += v
+		} else {
+			result += Title(v)
+		}
+	}
+	return result
+}
